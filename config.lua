97110de22c6dbbd8a915829f7734cdd561beab23
Andrew <63685025+starsweep@users.noreply.github.com>
2025-02-05 13:50:58 -0500

    Initial commit

---
+++ config.lua
@@ -0,0 +1,204 @@
+vim.o.clipboard = "unnamedplus"
+vim.o.number = true
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.termguicolors = true
+vim.o.wrap = true
+vim.o.showtabline = 2
+vim.o.syntax = "on"
+
+vim.cmd("set noswapfile")
+vim.cmd("set undofile")
+vim.cmd[[colorscheme tokyonight]]
+
+vim.cmd('syntax on')
+
+vim.loader.enable()
+
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.foldcolumn = '1'
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- Netrw Config
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+vim.g.netrw_winsize = 25
+vim.g.netrw_keepdir = 0
+vim.g.netrw_sort_sequence = [[[\/]$,*]]
+vim.g.netrw_keepdir = 0
+
+vim.cmd("hi! link netrwMarkFile Search")
+
+local function netrw_maps()
+  if vim.bo.filetype ~= "netrw" then
+    return
+  end
+
+  local opts = { silent = true }
+  -- Toggle dotfiles
+  vim.api.nvim_buf_set_keymap(0, "n", ".", "gh", opts)
+
+  -- Open file and close netrw
+  vim.api.nvim_buf_set_keymap(0, "n", "l", "<CR>:Lexplore<CR>", opts)
+
+  -- Open file or directory
+  vim.api.nvim_buf_set_keymap(0, "n", "o", "<CR>", opts)
+
+  -- Show netrw help in a floating (or maybe sidebar?) window
+  -- TODO: implement show_help function so we can implement this mapping
+  --[[ vim.api.nvim_buf_set_keymap(
+    0,
+    "n",
+    "?",Text File.txt
+    ":lua require('doom.core.settings.netrw').show_help()<CR>",
+    opts
+  ) ]]
+
+  -- Close netrw
+  vim.api.nvim_buf_set_keymap(0, "n", "q", ":Lexplore<CR>", opts)
+
+  -- Create a new file and save it
+  vim.api.nvim_buf_set_keymap(0, "n", "ff", "%:w<CR>:buffer #<CR>", opts)
+
+  -- Create a new directory
+  vim.api.nvim_buf_set_keymap(0, "n", "fa", "d", opts)
+
+  -- Rename file
+  vim.api.nvim_buf_set_keymap(0, "n", "fr", "R", opts)
+
+  -- Remove file or directory
+  vim.api.nvim_buf_set_keymap(0, "n", "fd", "D", opts)
+
+  -- Copy marked file
+  vim.api.nvim_buf_set_keymap(0, "n", "fc", "mc", opts)
+
+  -- Copy marked file in one step, with this we can put the cursor in a directory
+  -- after marking the file to assign target directory and copy file
+  vim.api.nvim_buf_set_keymap(0, "n", "fC", "mtmc", opts)
+
+  -- Move marked file
+  vim.api.nvim_buf_set_keymap(0, "n", "fx", "mm", opts)
+
+  -- Move marked file in one step, same as fC but for moving files
+  vim.api.nvim_buf_set_keymap(0, "n", "fX", "mtmm", opts)
+
+  -- Execute commands in marked file or directory
+  vim.api.nvim_buf_set_keymap(0, "n", "fe", "mx", opts)
+
+  -- Show a list of marked files and directories
+  vim.api.nvim_buf_set_keymap(
+    0,
+    "n",
+    "fm",
+    ':echo "Marked files:\n" . join(netrw#Expose("netrwmarkfilelist"), "\n")<CR>',
+    opts
+  )
+
+  -- Show target directory
+  vim.api.nvim_buf_set_keymap(
+    0,
+    "n",
+    "ft",
+    ':echo "Target: " . netrw#Expose("netrwmftgt")<CR>',
+    opts
+  )
+
+  -- Toggle the mark on a file or directory
+  vim.api.nvim_buf_set_keymap(0, "n", "<TAB>", "mf", opts)
+
+  -- Unmark all the files in the current buffer
+  vim.api.nvim_buf_set_keymap(0, "n", "<S-TAB>", "mF", opts)
+
+  -- Remove all the marks on all files
+  vim.api.nvim_buf_set_keymap(0, "n", "<Leader><TAB>", "mu", opts)
+
+  -- Create a bookmark
+  vim.api.nvim_buf_set_keymap(0, "n", "bc", "mb", opts)
+
+  -- Remove the most recent bookmark
+  vim.api.nvim_buf_set_keymap(0, "n", "bd", "mB", opts)
+
+  -- Jumo to the most recent bookmark
+  vim.api.nvim_buf_set_keymap(0, "n", "bj", "gb", opts)
+end
+netrw_maps()
+
+-- ----------
+
+lvim.plugins = {
+  {'nanozuki/tabby.nvim'},
+  {"kevinhwang91/nvim-ufo",
+      dependencies = {"kevinhwang91/promise-async"}
+  },
+  {'prichrd/netrw.nvim', opts = {}}
+}
+
+-- ----------
+
+require("netrw").setup({})
+
+-- ----------
+
+require('ufo').setup({
+  provider_selector = function(bufnr, filetype, buftype)
+    return {'treesitter', 'indent'}
+    end
+})
+
+-- ----------
+
+local theme = {
+  fill = 'TabLineFill',
+  -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
+  head = 'TabLine',
+  current_tab = 'TabLineSel',
+  tab = 'TabLine',
+  win = 'TabLine',
+  tail = 'TabLine',
+}
+require('tabby').setup({
+  line = function(line)
+    return {
+      {
+        { '  ', hl = theme.head },
+        line.sep('', theme.head, theme.fill),
+      },
+      line.tabs().foreach(function(tab)
+        local hl = tab.is_current() and theme.current_tab or theme.tab
+        return {
+          line.sep('', hl, theme.fill),
+          tab.is_current() and '' or '󰆣',
+          tab.number(),
+          tab.name(),
+          tab.close_btn(''),
+          line.sep('', hl, theme.fill),
+          hl = hl,
+          margin = ' ',
+        }
+      end),
+      line.spacer(),
+      line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+        return {
+          line.sep('', theme.win, theme.fill),
+          win.is_current() and '' or '',
+          win.buf_name(),
+          line.sep('', theme.win, theme.fill),
+          hl = theme.win,
+          margin = ' ',
+        }
+      end),
+      {
+        line.sep('', theme.tail, theme.fill),
+        { '  ', hl = theme.tail },
+      },
+      hl = theme.fill,
+    }
+  end,
+  -- option = {}, -- setup modules' option,
+})
